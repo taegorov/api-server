ae9a75960cd84b217f2cccb77c25fa0df9be544e
taegorov <taegorov@gmail.com>
2021-06-17 18:53:37 -0700

    passes tests, updates readme, etc

--- __tests__/collection.test.js
+++ __tests__/collection.test.js
@@ -1,6 +1,11 @@
 'use strict';
 
-const { db, customerCollection, orderCollection } = require('../src/models/index.js');
+const { db, data, customerCollection, orderCollection } = require('../src/models/index.js');
+const server = require('../src/server.js');
+const supertest = require('supertest');
+const { expect } = require('@jest/globals');
+
+const request = supertest(server.app);
 
 
 // below code block borrowed from Jacob Knaack of Code Fellows
@@ -12,9 +17,36 @@ afterAll(async () => {
 });
 
 
-
 describe('testing collection', () => {
-  it('should create a Customer and an Order', async () => {
+
+  let testCustomer = {
+    name: 'test customer',
+  }
+  let testOrder = {
+    name: 'test order',
+  }
+  let customer = null;
+  let customers = null;
+  let order = null;
+  let orders = null;
+
+
+  // === === 404 on a bad route === === //
+  test('Testing 404 on a bad route', async () => {
+    const response = await request.get('/badroute');
+    expect(response.status).toEqual(404);
+  });
+
+
+  // === === 404 on a bad method === === //
+  test('Testing 404 on a bad method', async () => {
+    const response = await request.put('/customer');
+    expect(response.status).toEqual(404);
+  });
+
+
+  // === === Creates a record === === //
+  test('Creates a customer and an order', async () => {
     let newCustomer = await customerCollection.create({ name: 'Tim' });
 
     expect(newCustomer.id).toEqual(1);
@@ -24,9 +56,34 @@ describe('testing collection', () => {
     expect(newOrder.name).toEqual('test order');
   });
 
-  it('shoud retrieve both Customers and Orders', async () => {
-    let customer = await customerCollection.read(1, { include: orderCollection.model });
+
+  // === === Reads a list of records === === //
+  test('Reads customers and orders', async () => {
+    let customer = await customerCollection.read();
     console.log(customer);
-    expect(customer.Orders).toBeTruthy();
+    expect(customer[0].name).toEqual('Tim');
+
   });
+
+
+  // === === Read an order from a particular customer === === //
+  test('Reads customers and orders', async () => {
+    let customer = await customerCollection.read(1, { include: orderCollection.model });
+    console.log('customer', customer.Orders);
+
+    expect(customer.Orders).toBeTruthy();
+    expect(customer.name).toEqual('Tim');
+
+  });
+
+
+  // === === Update an order for a customer === === //
+
+
+
+
+
+
+
+
 });
--- src/models/index.js
+++ src/models/index.js
@@ -7,7 +7,17 @@ const Collection = require('./collection.js');
 const { Sequelize, DataTypes } = require('sequelize');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
-const sequelize = new Sequelize(DATABASE_URL);
+
+// const sequelize = new Sequelize(DATABASE_URL);
+
+let sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
+});
 
 const orders = orderModel(sequelize, DataTypes);
 const customers = customerModel(sequelize, DataTypes);
@@ -15,6 +25,7 @@ const customers = customerModel(sequelize, DataTypes);
 
 const customerCollection = new Collection('customers', customers);
 const orderCollection = new Collection('orders', orders);
+
 
 customerCollection.createAssociation('hasMany', orderCollection.model, {
   foreignKey: 'customerId', sourceKey: 'id'
